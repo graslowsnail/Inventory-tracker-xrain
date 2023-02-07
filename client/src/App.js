e2cd99ea8ca13448bac253cade991d4167a6dc6b
graslowsnail <graslowsnail@gmail.,com>
2023-02-07 17:53:17 -0600

    added header from tailwind css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
+import Header from './components/Header';
 
 function App() {
   return (
+      <div>
 <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-green-700 font-bold mb-5">
         Welcome!
@@ -9,6 +11,7 @@ function App() {
         React and Tailwind CSS in action
       </p>
     </div>
+      </div>
   );
 }
 
