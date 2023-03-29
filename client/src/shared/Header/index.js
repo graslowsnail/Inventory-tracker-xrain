e51db01782fbb58b0819929b59a56dfbd291fda8
graslowsnail <graslowsnail@gmail.com>
2023-03-28 19:28:35 -0500

    added place holder for nav links.

--- client/src/shared/Header/index.js
+++ client/src/shared/Header/index.js
@@ -4,6 +4,18 @@ const Header = () => {
     return (
         <header className='main-header'>
             <h1 className='title'>X-Rain Inc. </h1>
+            
+            <ul className='nav-links'>
+                <li>
+                    ADD NEW PART
+                </li>
+                <li>
+                    PARTS
+                </li>
+                <li>
+                    AUTH
+                </li>
+            </ul>
         </header>
     );
 };
