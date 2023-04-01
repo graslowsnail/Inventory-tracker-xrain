bd1c02dcf7e80af5fb40dbef467cabe969f360c3
graslowsnail <graslowsnail@gmail.com>
2023-03-31 22:40:48 -0500

    quick change to header

--- client/src/shared/Header/NavLinks.js
+++ client/src/shared/Header/NavLinks.js
@@ -6,13 +6,13 @@ const NavLinks = props => {
   return (
     <ul className='nav-links'>
         <li>
-            <NavLink to='/parts'> PARTS </NavLink>
+            <NavLink to='/new/workday'> NEW WORKDAY </NavLink>
+        </li>
+        <li>
+            <NavLink to='/parts'> ALL PARTS </NavLink>
         </li>
         <li>
             <NavLink to='/new/parts'> NEW PART </NavLink>
-        </li>
-        <li>
-            <NavLink to='/new/workday'> NEW WORKDAY </NavLink>
         </li>
         <li>
             <NavLink to='/auth'> AUTHENTHICATE </NavLink>
