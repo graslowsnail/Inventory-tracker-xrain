c419985125261323d1ada9644dc5a4be7c5d9882
graslowsnail <graslowsnail@gmail.com>
2023-04-01 20:36:58 -0500

    added add part form, with validation. consol logs object when clicked

--- client/src/parts/pages/NewPart.js
+++ client/src/parts/pages/NewPart.js
@@ -1,8 +1,81 @@
+import React from 'react';
+
+import Input from '../../shared/FormElements/Input';
+import Button from '../../shared/FormElements/Button';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './PartForm.css';
+import { useForm } from '../../shared/hooks/form-hook';
 
 const NewPart = () => {
-  return (
-      <div className='center'> NEW PART FORM</div>
-  );
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: '',
+        isValid: false
+      },
+      size: {
+        value: '',
+        isValid: false
+      },
+      quantity: {
+        value: '',
+        isValid: false
+      },
+      partNumber: {
+        value: '',
+        isValid: false
+      }
+    }, false);
+
+const partSubmitHandler = event => {
+  event.preventDefault();
+  console.log(formState.inputs);// send this to the backend!!!!
 };
+
+return (
+  <form className='place-form' onSubmit={partSubmitHandler}>
+    <Input
+      id='name'
+      element='input'
+      type='text'
+      lable='Name'
+      validators={[VALIDATOR_REQUIRE() ]}
+      errorText='Please enter a valid title.'
+      onInput={inputHandler}
+    />
+      <Input 
+      id='size'
+      element='textarea'
+      lable='size'
+      validators={[VALIDATOR_MINLENGTH(5)]}
+      errorText= 'please enter a valid descrtiption (at least 5 char long).'
+      onInput={inputHandler}
+    />
+    <Input
+      id='quantity'
+      element='textarea'
+      lable='quantity'
+      validators={[VALIDATOR_MINLENGTH(1)]}
+      errorText='please enter quantity over 1'
+      onInput={inputHandler}
+    />
+    <Input
+      id='partNumber'
+      element='textarea'
+      lable='partNumber'
+      validators={[VALIDATOR_MINLENGTH(1)]}
+      errorText='please enter partNumber longer then  1 char'
+      onInput={inputHandler}
+    />
+
+    <button type='submit' disabled={!formState.isValid}>
+    ADD PART
+    </button>
+
+  </form>
+);
+};
+
+
 
 export default NewPart;
