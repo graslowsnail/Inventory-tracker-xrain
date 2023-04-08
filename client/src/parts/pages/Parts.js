48df76bd880052dc5b214add7022340e6ae82733
graslowsnail <graslowsnail@gmail.com>
2023-04-07 21:19:23 -0500

    fixed workday and parts components from infanitly fetching from backend

--- client/src/parts/pages/Parts.js
+++ client/src/parts/pages/Parts.js
@@ -1,19 +1,39 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PartList from '../componets/PartList.js';
 
 const Part = () => {
   const [parts, setParts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3002/api/parts')
-      .then(response => response.json())
-      .then(data => setParts(data))
-      .catch(error => console.error(error));
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch parts');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setParts(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
-  return (
-    < PartList items={parts} />
-  );
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return <PartList items={parts} />;
 };
 
 export default Part;
+
--- client/src/workDays/pages/WorkDay.js
+++ client/src/workDays/pages/WorkDay.js
@@ -3,13 +3,38 @@ import WorkDayList from '../componets/WorkDayList';
 
 const WorkDay = () => {
   const [workDays, setWorkDays] = useState([]);
-  fetch('http://localhost:3002/api/workdays')
-    .then(response => response.json())
-    .then(data => setWorkDays(data))
-    .catch(error => console.error(error));
-  return (
-    < WorkDayList items={workDays} />
-  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('http://localhost:3002/api/workdays')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch workdays');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setWorkDays(data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return <WorkDayList items={workDays} />;
 };
 
 export default WorkDay;
+
