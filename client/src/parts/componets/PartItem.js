ebccda900d779885fac02f540a7a44536a39a150
graslowsnail <graslowsnail@gmail.com>
2023-04-13 16:30:27 -0500

    added delete modal, and delete and edit button for individual parts

--- client/src/parts/componets/PartItem.js
+++ client/src/parts/componets/PartItem.js
@@ -1,15 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+//import { Link } from "react-router-dom";
 
 import Card from '../../shared/UIElements/Card';
+import Button from '../../shared/FormElements/Button';
+import Modal from '../../shared/UIElements/Modal';
 
 import "./PartItem.css";
 
 const PartItem = (props) => {
+    const [showConfirmModal, setShowConfirmModal ] = useState(false);
+
+    const showDeleteWarningHandler = () => {
+       setShowConfirmModal(true); 
+    };
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    };
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false);
+        console.log('...deleting');
+    };
+
+
   return (
+      <React.Fragment>
+      <Modal
+        show= {showConfirmModal}
+        onCancel= {cancelDeleteHandler}
+        header= 'are you sure' footerClass='part-item__modal-actions' footer={
+            <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>CANCLE</Button>
+            <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+            </React.Fragment>
+        }>
+      <p>do you wnat to proceed and delete this part? please note that this cannot be undone</p>
+      </Modal>
+
     <li className="part-item">
       <Card className="part-item__content">
-        <Link to={`parts/${props._id}`}>
           <div>
             <div className="part-item__info">
               <h2>{props.name}</h2>
@@ -18,9 +46,13 @@ const PartItem = (props) => {
               <h3>PARTNUMBER: {props.partNumber}</h3>
             </div>
           </div>
-        </Link>
+      <div>
+        <Button to={`/parts/${props.id}`}>EDIT</Button>
+        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+      </div>
       </Card>
     </li>
+      </React.Fragment>
   );
 };
 export default PartItem;
--- client/src/shared/UIElements/Modal/index.js
+++ client/src/shared/UIElements/Modal/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CSSTransition } from 'react-transition-group'
 
-import Backdrop from '../Backdrop';
+import Backdrop from '../Backdrop/Backdrop.js';
 import './Modal.css';
 
 const ModalOverlay = props => {
