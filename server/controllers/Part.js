985cbaa56304cc6bea243de842f59ad42603ee8b
graslowsnail <graslowsnail@gmail.com>
2023-04-27 12:55:52 -0500

    fixed the Update Part controller. The controller was updating the part twice, added error handling to the rest of the controllers in the Parts Controller file

--- server/controllers/Part.js
+++ server/controllers/Part.js
@@ -4,8 +4,14 @@ const Part = require('../models/Part.js');
 
 // GET all parts
 const getParts = async (req, res) => {
+    try {
     const parts = await Part.find();
     res.send(parts);
+    } catch(err) {
+        console.log('########');
+        console.log( err.message)
+        res.status(500).send({ error: err.message })
+    }
 };
 
 //GET a single part by id
@@ -16,11 +22,13 @@ const getPartById = async (req, res) => {
     } catch {
         res.status(404);
         res.send({ error: 'Part doesnt exist!'});
+        res.status(500).send({ error: err.message })
     }
 };
 
 // CREATE part
 const createPart = async(req, res) => {
+    try {
     const part = new Part ({
         name: req.body.name,
         size: req.body.size,
@@ -29,6 +37,11 @@ const createPart = async(req, res) => {
     })
     await part.save()
     res.send(part);
+    } catch(err) {
+        console.log('########');
+        console.log( err.message)
+        res.status(500).send({ error: err.message })
+    }
 };
 
 // DELETE a part
@@ -56,11 +69,12 @@ const updatePart = async(req, res) => {
         part.size = req.body.size
         part.quantity = req.body.quantity
         part.barCodeId = req.body.barCodeId
-
-        await Part.findOneAndUpdate({ _id: req.params.id }, part, { new: true })
-
+        //await Part.findOneAndUpdate({ _id: req.params.id }, part, { new: true })
+        await part.save();
         res.send(part)
     } catch (err) {
+        console.log('########');
+        console.log( err.message)
         res.status(500).send({ error: err.message })
     }
 };
