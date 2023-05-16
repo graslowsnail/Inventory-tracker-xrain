1394a372b884581a422dc6ed3db0bf63e9734e5d
graslowsnail <graslowsnail@gmail.com>
2023-05-16 12:47:16 -0500

    changed styling on the workday list. added tailwind grid component.

--- client/src/workDays/componets/WorkDayList.js
+++ client/src/workDays/componets/WorkDayList.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import WorkDayItem from './WorkDayItem.js';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 
-import '../../parts/componets/PartList.css';
+const WorkDayList = (workDays) => {
 
-const WorkDayList = ( workDays ) => {
-    
-    //FORMATTED CREATED AT DATE
     const formattedDate = moment(workDays.date).format('MM/DD/YYYY'); // returns a string in the format of MM/DD/YYYY
 
-  return(
-<ul className='part-list'>
-      {workDays.items?.map(workDay =>(
-        < WorkDayItem
-          key={workDay._id} 
-          id={workDay._id}
-          name={workDay.name}
-          partsUsed={workDay.partsUsed}
-          date={formattedDate}
-        />
+  return (
+    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {workDays.items?.map((workDay) => (
+        <li key={workDay._id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+      <Link to={`/workday/${workDay._id}`}>
+          <div className="flex w-full items-center justify-between space-x-6 p-6">
+            <div className="flex-1 truncate">
+              <div className="flex items-center space-x-3">
+                <h1 className="truncate text-sm font-medium text-gray-900">{workDay.name}</h1>
+              </div>
+              <p className="mt-1 truncate text-sm text-gray-500">{formattedDate}</p>
+            </div>
+          </div>
+          </Link>
+        </li>
       ))}
     </ul>
-  );
-};
+  )
+}
 
 export default WorkDayList;
