683759ab62ba226adae6e05f7144feebb475ca76
graslowsnail <graslowsnail@gmail.com>
2024-06-02 20:36:18 -0700

    render the work day page again, was rendering test analytics page for pipeline

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import '../src/analytics/pages/Scrollbar.css';
 import MainNavigation from './shared/Header/MainNavigation';
 import NewPart from './parts/pages/NewPart';
 import Part from './parts/pages/Parts';
-//import WorkDay from './workDays/pages/WorkDay';
+import WorkDay from './workDays/pages/WorkDay';
 import NewWorkDay from './workDays/pages/NewWorkDay';
 import UpdatePart from './parts/pages/UpdatePart';
 import SingleWorkDay from './workDays/pages/singleWorkDay';
 import PartHistory from './parts/pages/PartsHistory';
 import Login from './login/Login.jsx';
-import AnalyticsPage from './analytics/pages/AnalyticsPage.js';
+//import AnalyticsPage from './analytics/pages/AnalyticsPage.js';
 
 const App = () => {
 
@@ -22,7 +22,7 @@ const App = () => {
       <main>
           <Routes>
               {/*workday routes bellow */}
-              <Route exact path='/' element={<AnalyticsPage/>}/>
+              <Route exact path='/' element={<WorkDay/>}/>
               <Route exact path='/new/workday' element={<NewWorkDay/>} />
               <Route exact path='/workday/:workDayId' element={<SingleWorkDay/>} />
               {/*part routes bellow */}
